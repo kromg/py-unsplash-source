2b5c67901070c9397c0a1ead9c3290b46ae9065b
Giacomo Montagner <kromg.kromg@gmail.com>
2019-03-29 15:49:40 +0100

    Repository initialization

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-unsplash-source',
+    version='0.1dev',
+    packages=find_packages(
+        exclude=['tests']
+    ),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    zip_safe=True,
+    platforms="POSIX",
+    test_require=['pytest'], install_requires=['pytest']
+)
