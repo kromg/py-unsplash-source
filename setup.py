3da171223534e4f8bc5b3d78c8863fbbec09ea1a
Giacomo Montagner <kromg.kromg@gmail.com>
2019-03-29 16:10:11 +0100

    Rename of package with underscores

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py-unsplash-source',
+    name='py_unsplash_source',
     version='0.1dev',
     packages=find_packages(
         exclude=['tests']
